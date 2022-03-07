fa92eb29c0d90a5084e7f4f5e8b4c9344a166e62
Muhammad Ghazian <mghazian@github.com>
2022-03-07 12:56:01 +0700

    Finish up chapter about input reception

---
+++ app3.py
@@ -0,0 +1,6 @@
+# 25:31 Menerima Input User
+name = input("Siapa nama kamu? ")
+hobby = input("Apa hobimu? ")
+sentence = name + " Selamat belajar Python"
+print (sentence)
+print (name + " hobimu adalah " + hobby)
