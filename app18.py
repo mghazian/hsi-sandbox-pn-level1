d7da8681e6f447903a592bb9e0a5564b5ec75902
Muhammad Ghazian <mghazian@github.com>
2022-03-07 23:32:58 +0700

    Finish up list chapter

---
+++ app18.py
@@ -0,0 +1,10 @@
+# 2:05:52 List
+names = ["Bono", "Uganda", "Nababan", "Maul"]
+
+print(names)
+print(names[-1])
+print(names[1:3])
+print(names[1::2])
+
+for name in names:
+    print(f"Nama: {name}")
