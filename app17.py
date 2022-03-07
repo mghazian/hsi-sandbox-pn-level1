5d98e7282c76046f0b339207ae3b2e2f6c4c6665
Muhammad Ghazian <mghazian@github.com>
2022-03-07 23:27:53 +0700

    Finish up for loop chapter

---
+++ app17.py
@@ -0,0 +1,11 @@
+# 2:00:17 Perulangan For
+name = "Bono"
+for i in name:
+    print(i)
+
+numbers = [1, 2, 3, 4, 5]
+for val in numbers:
+    print(val)
+
+for item in range(1, 11, 2):
+    print(item)
