bbb47c94ee850b31861aab199c44d9c7bbd4d6c9
Muhammad Ghazian <mghazian@github.com>
2022-03-07 21:20:06 +0700

    Finish up comparison operator chapter

---
+++ app12.py
@@ -0,0 +1,31 @@
+# 1:23:39 Operator Perbandingan
+result = 4 > 3
+print(result)
+
+result = 4 > 3
+print(result)
+
+result = 4 >= 3
+print(result)
+
+result = 4 <= 3
+print(result)
+
+result = 4 == 3
+print(result)
+
+result = 4 == 4
+print(result)
+
+result = 4 != 4
+print(result)
+
+grade = 5
+if grade >= 9:
+    print("Nilai kamu A")
+elif grade >= 7:
+    print("Nilai kamu B")
+elif grade >= 6:
+    print("Nilai kamu C")
+else:
+    print("Silahkan belajar lagi dengan tekun")
