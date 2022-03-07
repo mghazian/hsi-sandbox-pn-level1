3fbec062d01cefe0173bd187e79e48b2f0c8802f
Muhammad Ghazian <mghazian@github.com>
2022-03-07 09:00:18 +0700

    Add first tutorial section

---
+++ pertama.py
@@ -0,0 +1,15 @@
+# 5:09 Program Python Pertama
+# hello world
+print("hello world")
+
+# hello
+print("hello")
+
+# world
+print("world")
+
+# 100
+print(100)
+
+# Python Python Python 
+print("Python " * 3) # bruh
