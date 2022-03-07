e2cce0ef53b4eb9c3787d683f895e1ed5a69a931
Muhammad Ghazian <mghazian@github.com>
2022-03-07 09:19:28 +0700

    Finish up chapter about variables

---
+++ app2.py
@@ -0,0 +1,22 @@
+# 13:13 Variabel dan Tipe Data
+price = 100000
+print(price) # 100000
+
+price = 50000
+print (price) # 50000
+
+price = "harga"
+print (price) # harga
+
+discount = 0.5
+print (discount) # 0.5
+
+price = 100000 * discount
+print (price) # 50000.0
+
+product_name = "Gundam"
+print(product_name) # Gundam
+
+is_discount = True
+print (is_discount) # True
+
