ce75d25adfb5638a2236d30af04ba101693ab2b9
Muhammad Ghazian <mghazian@github.com>
2022-03-07 18:10:19 +0700

    Finish up math operation chapter

---
+++ app8.py
@@ -0,0 +1,18 @@
+# 1:04:24 Matematika
+x = 10
+y = 4
+print (x + y) # addition
+print (x - y) # subtraction
+print (x * y) # multiplication
+print (x / y) # division as float
+print (x // y) # division as int
+print (x ** y) # exponentiation
+print (x % y) # modulus
+
+x = x + 5
+print(x)
+
+x += 5
+print(x)
+x -= 5
+print(x)
