6d53c1fed2262e92a6a1e9884d3521f745e483eb
Muhammad Ghazian <mghazian@github.com>
2022-03-07 13:17:43 +0700

    Finish up formatted string chapter

---
+++ app6.py
@@ -0,0 +1,12 @@
+# 48:46 Formatted String
+first_name = "Bono"
+last_name = "Uganda"
+message = first_name + " [" + last_name + "]"
+print(message)
+
+message = f"{first_name} [{last_name}]"
+print(message)
+
+age = 31
+message = f"Umur kamu {age}"
+print(message)
