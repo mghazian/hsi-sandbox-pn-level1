391b7f52182f839ad819a0fc3a2517be47e10a2c
Muhammad Ghazian <mghazian@github.com>
2022-03-07 21:13:16 +0700

    Finish up if statement subject

---
+++ app11.py
@@ -0,0 +1,11 @@
+# 1:16:43 Percabangan If
+is_day = True
+is_night = True
+if is_day:
+    print("Selamat siang")
+elif is_night:
+    print("Selamat malam")
+else:
+    print("Selamat suka-suka")
+
+print ("Selamat menikmati harimu")
