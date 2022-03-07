195b5883e86389bd79a35db6f40e01f3cc263c7a
Muhammad Ghazian <mghazian@github.com>
2022-03-07 23:38:33 +0700

    Finish up list method chapter

---
+++ app19.py
@@ -0,0 +1,20 @@
+# 2:10:50 List Method
+numbers = [5, 6, 7, 8, 1]
+
+numbers.append(99)
+print(numbers)
+
+numbers.insert(3, 'aaa')
+print(numbers)
+
+numbers.remove('aaa')
+print(numbers)
+
+numbers.pop()
+print(numbers)
+
+numbers.pop(2)
+print(numbers)
+
+numbers.sort()
+print(numbers)
