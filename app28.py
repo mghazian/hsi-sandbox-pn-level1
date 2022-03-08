f98275719fa035aadc009d63f08224e3a3d31ffd
Muhammad Ghazian <mghazian@github.com>
2022-03-08 14:10:38 +0700

    Finish up function parameter chapter

---
+++ app28.py
@@ -0,0 +1,10 @@
+# 3:00:02 Parameter Fungsi
+def halo_user(name = "user", level = 0):
+    print(f"Halo {name} - {level}")
+    print("Selamat belajar Python")
+
+print("Start")
+halo_user("Bono", 4)
+print("=======================")
+halo_user("Uganda", 2)
+print("Finish")
