122cbc42478e899091e79439ac1db1fed07866e7
Muhammad Ghazian <mghazian@github.com>
2022-03-08 14:14:29 +0700

    Finish up keyword argument chapter

---
+++ app29.py
@@ -0,0 +1,8 @@
+# 3:04:52 Keyword Argument
+def halo_user(name = "user", level = 0):
+    print(f"Halo {name} - {level}")
+    print("Selamat belajar Python")
+
+print("Start")
+halo_user(level = 10, name = "Bono")
+print("Finish")
